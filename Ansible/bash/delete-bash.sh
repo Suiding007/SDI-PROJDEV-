51e325d694a0565ec6c1622841c33d6550d7f62a
Super geheim <xAlta@users.noreply.github.com>
2024-11-25 14:26:19 +0100

    Delete Ansible/bash directory

--- Ansible/bash/delete-bash.sh
+++
@@ -1,5 +0,0 @@
-mysql -u root -pWelkom123 <<EOF
-DROP DATABASE moodle;
-DROP USER 'moodledude'@'localhost';
-
-EOF
--- Ansible/bash/mysql.sh
+++
@@ -1,8 +0,0 @@
-mysql -u root -pWelkom123 <<EOF
-#DROP DATABASE moodle;
-CREATE DATABASE moodle DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-create user 'moodledude'@'localhost' IDENTIFIED BY 'passwordformoodledude';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, CREATE TEMPORARY TABLES, DROP, INDEX, ALTER ON moodle.* TO 'moodledude'@'localhost';
-FLUSH PRIVILEGES;
-EOF
-
