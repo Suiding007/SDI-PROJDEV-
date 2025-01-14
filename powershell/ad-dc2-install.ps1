df591f56f4dd7d84255b65c309abb24fc5aea272
suiding007 <61475324+Suiding007@users.noreply.github.com>
2025-01-14 14:41:15 +0100

    new powershell scripts
    adding migration scripts for ad, dns and dhcp

---
+++ powershell/ad-dc2-install.ps1
@@ -0,0 +1,16 @@
+$setup = @{
+    DomainName = "knaak-hosting.nl"
+    Credential = (Get-Credential "KNAAK-HOSTING\Administrator")
+    SiteName = "Default-First-Site-Name"
+    InstallDns = $True
+    NoGlobalCatalog = $false
+    CreateDnsDelegation = $false 
+    CriticalReplicationOnly = $false
+    DatabasePath = "C:\Windows\NTDS"
+    LogPath = "C:\Windows\NTDS"
+    SysvolPath = "C:\Windows\SYSVOL"
+    NoRebootOnCompletion = $false 
+    ReplicationSourceDC = "ad0-knaak.knaak-hosting.nl"
+    Force = $true
+}
+Install-ADDSDomainController @setup
---
+++ powershell/dns-migration-server2.ps1
@@ -0,0 +1,1 @@
+Add-DnsServerSecondaryZone -Name "ijsselstreek-university.nl" -ZoneFile "ijsselstreek-university.dns" -MasterServers 10.8.0.10
