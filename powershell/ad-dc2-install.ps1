6ff57dab4d8f68fe806420197cf64ad43a0a5122
suiding007 <61475324+Suiding007@users.noreply.github.com>
2025-01-16 16:28:36 +0100

    Update ad-dc2-install.ps1

--- powershell/ad-dc2-install.ps1
+++ powershell/ad-dc2-install.ps1
@@ -1,6 +1,6 @@
 $setup = @{
     DomainName = "knaak-hosting.nl"
-    Credential = (Get-Credential "KNAAK-HOSTING\Administrator")
+    Credential = (Get-Credential "KNAAK-HOSTING\KnaakAdmin")
     SiteName = "Default-First-Site-Name"
     InstallDns = $True
     NoGlobalCatalog = $false
